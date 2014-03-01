c95e40d7397c51894dce8bc3913a9646a1851fa6
Minh Khue <lmk19922000@gmail.com>
2014-03-02 04:16:21 +0800

    Problem 2 Works Applications test

--- src/jp/co/wap/exam/Problem2.java
+++ src/jp/co/wap/exam/Problem2.java
@@ -4,8 +4,88 @@ import java.util.List;
 import jp.co.wap.exam.lib.Interval;
 
 public class Problem2 {
+	int[] compatibleIntervals;
+	int[] maxWorkingTimes;
+	Interval[] times; // array of sorted intervals
+
+	/**
+	 * 
+	 * @author Le Minh Khue
+	 * 
+	 *         Idea: First sort the intervals based on end time. Then use
+	 *         dynamic programming to compute the max working time possible
+	 */
 	public int getMaxWorkingTime(List<Interval> intervals) {
-		// TODO: Implement this method.
-		return 0;
+		int[] countArray = new int[1441]; // There are at most 24*60+1 = 1440
+											// different times
+		times = new Interval[intervals.size()];
+
+		int timeInMinute;
+
+		// Calculating frequencies
+		for (int i = 0; i < intervals.size(); i++) {
+			timeInMinute = intervals.get(i).getEndMinuteUnit();
+			countArray[timeInMinute]++;
+		}
+
+		// Accumulating
+		int total = 0;
+		for (int i = 0; i < countArray.length; i++) {
+			countArray[i] = countArray[i] + total;
+			total = countArray[i];
+		}
+
+		// Sort intervals based on end time
+		for (int i = intervals.size() - 1; i >= 0; i--) {
+			timeInMinute = intervals.get(i).getEndMinuteUnit();
+			countArray[timeInMinute]--;
+			times[countArray[timeInMinute]] = intervals.get(i);
+		}
+
+		// Initialization
+		compatibleIntervals = new int[intervals.size()];
+		for (int i = 0; i < compatibleIntervals.length; i++) {
+			compatibleIntervals[i] = -1;
+		}
+
+		// For each interval i, find largest interval j that is compatible with
+		// i such that j<i
+		for (int i = 0; i < times.length; i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (times[j].getEndMinuteUnit() < times[i].getBeginMinuteUnit()) {
+					compatibleIntervals[i] = j;
+					break;
+				}
+			}
+		}
+
+		maxWorkingTimes = new int[intervals.size()];
+
+		return computeMaxWorkingTime(intervals.size() - 1);
+	}
+
+	/**
+	 * 
+	 * @author Le Minh Khue
+	 * 
+	 *         Dynamic programming maxWorkingTime(i) is the maxWorkingTime
+	 *         consisting of intervals from 0 up to and include i
+	 * 
+	 *         maxWorkingTime(i) = interval (0) if i == 0
+	 *         maxWorkingTime(i) = max (maxWorkingTime(i-1), maxWorkingTime(compatibleIntervals[i])+interval(i)) otherwise
+	 * 
+	 */
+	private int computeMaxWorkingTime(int index) {
+		if (index < 0) {
+			return 0;
+		}
+
+		if (maxWorkingTimes[index] != 0) {
+			return maxWorkingTimes[index];
+		}
+
+		return Math.max(computeMaxWorkingTime(index - 1),
+				computeMaxWorkingTime(compatibleIntervals[index])
+						+ times[index].getIntervalMinute());
 	}
 }
--- src/jp/co/wap/exam/TestProblem.java
+++ src/jp/co/wap/exam/TestProblem.java
@@ -13,18 +13,15 @@ public class TestProblem {
 	@Test
 	public void testProblem1Usage() {
 		Problem1 p = new Problem1();
-		
-		List<Interval> figure0 = Arrays.asList(new Interval("08:00", "10:00"), new Interval("09:00", "11:00"), new Interval("10:00", "12:00"),
+		List<Interval> lists = Arrays.asList(new Interval("08:00", "10:00"), new Interval("09:00", "11:00"), new Interval("10:00", "12:00"),
 												new Interval("10:00", "24:00"), new Interval("00:00", "08:00"), new Interval("11:00", "12:00"),
 												new Interval("17:00", "18:00"), new Interval("17:00", "19:00"), new Interval("17:30", "19:00"),
 												new Interval("16:00", "20:00"), new Interval("09:00", "13:00"), new Interval("09:00", "11:00"),
 												new Interval("09:00", "24:00"), new Interval("16:00", "17:30"), new Interval("13:00", "18:00"));
 		long startTime = System.nanoTime();
-		//System.out.println(p.getMaxIntervalOverlapCount(figure0));
-		p.getMaxIntervalOverlapCount(figure0);
+		p.getMaxIntervalOverlapCount(lists);
 		long endTime = System.nanoTime();
-
-		System.out.println(endTime - startTime);
+		//System.out.println(endTime - startTime);
 		
 		// example: Figure 1
 		Interval interval1 = new Interval("08:00", "12:00");
@@ -38,5 +35,16 @@ public class TestProblem {
 				new Interval("06:00", "09:30"), new Interval("12:00", "14:30"),
 				new Interval("10:00", "10:30"), new Interval("11:00", "13:30"));
 		assertEquals(p.getMaxIntervalOverlapCount(figure2), 3);
+	}
+	
+	@Test
+	public void testProblem2Usage() {
+		Problem2 p = new Problem2();
+		
+		List<Interval> lists = Arrays.asList(new Interval("06:00", "08:30"), new Interval("09:00", "11:30"), new Interval("12:30", "14:00"),
+											new Interval("08:00", "09:00"), new Interval("09:00", "11:00"), new Interval("10:30", "14:00"),
+											new Interval("18:00", "21:00"), new Interval("17:30", "18:30"), new Interval("20:40", "21:30"),
+											new Interval("23:00", "24:00"));
+		System.out.println(p.getMaxWorkingTime(lists));
 	}
 }
