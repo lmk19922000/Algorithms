e984f69b42b8b08b35ec241e310eadd556d23e49
Minh Khue <lmk19922000@gmail.com>
2014-03-01 21:44:55 +0800

    Minor changes to multiyply without * and +

--- src/Misc/MultiplyWithoutOperator.java
+++ src/Misc/MultiplyWithoutOperator.java
@@ -1,20 +1,34 @@
 package Misc;
 
+/**
+ * 
+ * @author Le Minh Khue
+ * The algorithm perform the traditional multiplication of 2 integers in binary form
+ * (take each digit of the second number and multiply it with the first number, then we add up the results together)
+ *  
+ * The algorithm first convert the integer into binary string representing it
+ * Then for each digit in the binary representation, it is multiplied with 1111 (binary representation of 15)
+ * (The multiplication does not require * or + operators because we can only have 2 results: 1111 or 0000
+ * Then we perform the addition of the results to get the final answer (in binary form)
+ * Finally, we convert the binary form to integer.
+ *
+ */
 public class MultiplyWithoutOperator {
 	public static void main(String[] args) {
-		int num = 3; // the number we want to multiply with 15
+		int num = 4; // the number we want to multiply with 15
 		
 		// Convert the number to binary form
-		String str = Integer.toBinaryString(num);
+		String binaryRepresentation = Integer.toBinaryString(num);
 
-		int strLength = str.length();
+		int strLength = binaryRepresentation.length();
 		int strPaddedLength = 3 + strLength;
 
-		// Array holding the product of each bit with 15
+		// Array holding the product of each bit with 1111
 		String[] partialSums = new String[strLength];
-
+		
+		// Pad the binary string in order for them to perform addition
 		for (int i = 0; i < strLength; i++) {
-			if (str.charAt(strLength - i - 1) == '1') {
+			if (binaryRepresentation.charAt(strLength - i - 1) == '1') {
 				partialSums[i] = pad("1111", i, strLength - 1);
 			} else {
 				partialSums[i] = pad("0000", i, strLength - 1);
