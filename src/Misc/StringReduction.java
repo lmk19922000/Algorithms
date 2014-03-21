cf47695b54fae8ae03c56aacb0884ce6db1267f2
Minh Khue <lmk19922000@gmail.com>
2014-03-22 04:59:48 +0800

    String reduction algorithm

---
+++ src/Misc/StringReduction.java
@@ -0,0 +1,54 @@
+package Misc;
+
+import java.util.Stack;
+
+public class StringReduction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String str = "aaaabbbca";
+		System.out.print(reduction(str));
+
+	}
+	
+	public static String reduction (String str){
+		StringBuilder ans = new StringBuilder("");
+		char curr, temp;
+		Stack<Character> stack = new Stack<Character>();
+		
+		for (int i = 0; i<str.length(); i++){
+			curr = str.charAt(i);
+			while (!stack.isEmpty()){
+				if (stack.peek() != curr){
+					temp = stack.pop();
+					curr = reduct(temp, curr);
+				} else{
+					break;
+				}
+			}
+			
+			stack.push(curr);
+		}
+		
+		while (!stack.isEmpty()){
+			ans.insert(0, stack.pop());
+		}
+		
+		return ans.toString();
+	}
+
+	private static char reduct(char temp, char curr) {
+		if ((temp == 'a' && curr == 'b') || (temp == 'b' && curr == 'a'))
+			return 'c';
+		
+		if ((temp == 'a' && curr == 'c') || (temp == 'c' && curr == 'a'))
+			return 'b';
+		
+		if ((temp == 'c' && curr == 'b') || (temp == 'b' && curr == 'c'))
+			return 'a';
+		
+		return 'd';
+	}
+}
